ab54321d1e7bac15ebff8551bd817d08bb1a8ff5
Darrell <darrell.anderson@gmail.com>
2022-10-23 09:48:35 -0700

    clean up event, use it in index.html

--- static/refresh.js
+++ static/refresh.js
@@ -1,20 +1,23 @@
+/**
+ * Fetch gamedata json at regular inverals, using last-modified timestamp to
+ * avoid processing already-delivered results.
+ *
+ * Include this script and register an 'onGameDataUpdate' event listener:
+ *
+ * window.addEventListener("onGameDataUpdate", (json) => {
+ *   console.log("onGameDataUpdate");
+ * });
+ */
+
 let _lastModified = "n/a";
-let _loopFetchHandle = undefined;
 
-// How to listen for updates:
-window.addEventListener("onLoopFetchResult", (json) => {
-  console.log("onLoopFetchResult");
-});
-
-const loopFetch = async () => {
-  await fetch("/data?key=buddy", {
+const loopFetch = () => {
+  fetch("/data?key=buddy", {
     cache: "no-cache",
     mode: "no-cors",
     headers: { "If-Modified-Since": _lastModified },
   }).then((response) => {
-    console.log(`response status ${response.status}`);
     if (response.status === 304) {
-      console.log("loopFetch not modified");
       return; // not modified
     } else if (response.status !== 200) {
       console.error(`loopFetch response status ${response.status}`);
@@ -24,23 +27,18 @@ const loopFetch = async () => {
     const lastModified = response.headers.get("Last-Modified");
     if (lastModified && lastModified === _lastModified) {
       // Cache gave us the same result as last time, ignore it.
-      console.log("loopFetch last-modified unchanged");
       return;
     }
     _lastModified = lastModified;
 
     response.json().then((json) => {
-      console.log(`loopFetch success, lastModified: "${_lastModified}`);
-      const event = new CustomEvent("onLoopFetchResult", json);
+      const event = new CustomEvent("onGameDataUpdate", json);
       window.dispatchEvent(event);
     });
   });
 };
 
 window.addEventListener("DOMContentLoaded", (window, event) => {
-  console.log("loopFetch start");
-  _loopFetchHandle = setInterval(loopFetch, 3000);
-
-  // Also run right away.
-  loopFetch();
+  setInterval(loopFetch, 3000);
+  loopFetch(); // run immediately too
 });
