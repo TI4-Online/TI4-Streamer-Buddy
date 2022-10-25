a3135841bdf867567637bb98f490b342013c83b0
Darrell <darrell.anderson@gmail.com>
2022-10-25 09:52:38 -0700

    split out error handler (does not prevent console log of 404)

--- static/refresh.js
+++ static/refresh.js
@@ -4,38 +4,55 @@
  *
  * Include this script and register an 'onGameDataUpdate' event listener:
  *
- * window.addEventListener("onGameDataUpdate", (json) => {
- *   console.log("onGameDataUpdate");
+ * window.addEventListener("onGameDataUpdate", (event) => {
+ *   console.log(`onGameDataUpdate ${event.detail.timestamp}`);
  * });
  */
 
 let _lastModified = "n/a";
+
+const processResponse = (response) => {
+  // Check if the response is "not modified" from our if-modified-since request.
+  if (response.status === 304) {
+    return; // not modified
+  }
+
+  // Stop if error.  Report the status code if other than "not found"
+  if (response.status !== 200) {
+    if (response.status !== 404) {
+      console.error(`loopFetch response status ${response.status}`);
+    }
+    return;
+  }
+
+  // The above 304 check fails if the cache rewrites it to a 200 using cached
+  // content.  Consult the last-modified time, only process if different.
+  const lastModified = response.headers.get("Last-Modified");
+  if (lastModified && lastModified === _lastModified) {
+    return;
+  }
+  _lastModified = lastModified; // remember
+
+  // Report update to listeners via a custom event. The event extra data
+  // MUST be stored using the reserved key 'detail'.
+  response.json().then((json) => {
+    const event = new CustomEvent("onGameDataUpdate", { detail: json });
+    window.dispatchEvent(event);
+  });
+};
+
+const processError = (error) => {
+  console.log(`ERROR "${error}`);
+};
 
 const loopFetch = () => {
   fetch("/data?key=buddy", {
     cache: "no-cache",
     mode: "no-cors",
     headers: { "If-Modified-Since": _lastModified },
-  }).then((response) => {
-    if (response.status === 304) {
-      return; // not modified
-    } else if (response.status !== 200) {
-      console.error(`loopFetch response status ${response.status}`);
-      return;
-    }
-
-    const lastModified = response.headers.get("Last-Modified");
-    if (lastModified && lastModified === _lastModified) {
-      // Cache gave us the same result as last time, ignore it.
-      return;
-    }
-    _lastModified = lastModified;
-
-    response.json().then((json) => {
-      const event = new CustomEvent("onGameDataUpdate", json);
-      window.dispatchEvent(event);
-    });
-  });
+  })
+    .then(processResponse)
+    .catch(processError);
 };
 
 window.addEventListener("DOMContentLoaded", (window, event) => {
